d2ea13f990ea6985195b8db5bef8da8cf90a52ce
github username <github email address>
2017-04-14 16:00:43 -0400

    Hey Corinna, this where we left off!

--- lib/soundly/cli.rb
+++ lib/soundly/cli.rb
@@ -2,53 +2,51 @@ class Soundly::CLI
 
 	def call
 		greetings
-		list_songs
 		menu
 	end
 
-	def list_songs
-		@songs = Soundly::Trending.today
-		@songs.each.with_index(1) do |song, i|
-			puts "#{i}. #{song}"
-		end
-	end
+	# def list_songs
+	# 	@songs = Soundly::Trending.today
+	# 	@songs.each.with_index(1) do |song, i|
+	# 		puts "#{i}. #{song}"
+	# 	end
+	# end
 
 	def greetings
 		puts "Hi there human. Lend me your earholes, I'll fill them will 👽"
-		puts "Todays rando pick is #{blah blah} by #{blah blah blah} coming in at #{song number} from todays top 50 on
-		SoundCloud."
+		puts "Todays rando pick is (blah) blah by (blah blah blah) coming in at (blah blahs song number) from todays top 50 on Soundcloud."
+		sleep 1
 	end
 
-	# def menu
-	# 	puts "Type "1" to list todays top 50 trending songs on SoundCloud"
-	# 	puts "Type "2" to list another random song from today's top 50"
-	# 	puts "Type "3" to exit"
-	# 	user_input = nil
-	# 	while user_input != "exit"
-	# 		user_input = gets.strip
-	# 		case user_input
-	# 		when "more"
-	# 			puts #(clears screen?) and prints more info on random song mentioned above
-	# 			puts "Type back to go back to Main Menu"
-	# 			user_input = gets.strip
-	# 			if user_input == back
-	# 				#back to main menu
-	# 			else
-	# 				puts "Silly human. That's not an optuon. Try again."
-	# 			end
-	# 		when "1"
-	# 			list_songs
-	# 			#logic some where in here to give the option to see details on a specific song
-	# 		when "2"
-	# 			#prints another random song with all of its information
-	# 		when "3"
-	# 			exit
-	# 		else
-	# 			puts "Silly human. That's not an optuon. Try again."
-	# 		end
-	# 		break if user_input == exit
-	# 	end
-	# end
+	def menu
+		user_input = nil
+		array = 1..50
+		# binding.pry
+
+		while user_input != "exit"
+			puts "Type 1 to list todays top 50 trending songs on SoundCloud"
+			puts "Type 2 to select specific song"
+			puts "Type '3' to list another random song from today's top 50"
+			puts "Type 'exit' to exit"
+
+			user_input = gets.strip.downcase
+
+			case user_input
+			when "1"
+				puts "here is todays top list"
+				# list_songs
+			when "2"
+				puts "which song would you like to select? (Select 1-50)"
+				puts "(This parts a bit tricky?)"
+			when "3"
+				puts "Here's another random song, Human"
+			when "exit"
+				exit
+			else
+				puts "Silly human. That's not an optuon. Try again."
+			end
+		end
+	end
 
 	# def menu
 	# 	input = nil
